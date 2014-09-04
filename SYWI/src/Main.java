043b9832994282e06882278e2752381055be0838
marcin2xp <marcinxp@gmail.com>
2014-09-04 10:57:38 +0200

    Działa czyszczenie nadmiarowych spacji

--- SYWI/src/Main.java
+++ SYWI/src/Main.java
@@ -1,14 +1,21 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.sql.Time;
 import java.util.Arrays;
+import java.util.Date;
+import java.util.Map;
 
+import orbital.logic.imp.Formula;
+import orbital.moon.logic.*;
 public class Main {
 	//Dane wykorzystane w programie posiadaja wszystkie wartosci jako flexible oraz jedna wartosc typu decision.
 	private static String[][] valuesEatable; //(X alfa) valuesEatable[i][0] is the favorable decision value d2
 	private static String[][] valuesPoison; //(X beta)
 	
 	private static String[][] reductTable; //Tabela po wykonaniu znajdowaniu reduktow
+	
+	private static Map<String, Integer> countReductMap;
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -26,8 +33,11 @@ public class Main {
 		//printTable(valuesEatable);
 		//printTable(valuesPoison);
 		findReduct(valuesEatable, valuesPoison);
+		//printTable(reductTable);
+		removeSpace(reductTable);
 		printTable(reductTable);
-		//System.out.println(reductTable[59][59]);
+		//System.out.println(reductTable[25][2]);
+		//CNFtoDNF();
 
 	}
 	
@@ -52,14 +62,12 @@ public class Main {
 		for(int i=0; i< values.length; i++){
 		    for(int j=0; j< values[i].length; j++){
 		    	System.out.println("\n------------------kolumna "+ j + " wiersz "+ i);
-		    	System.out.print(values[i][j]);	
-		    	
-		    }
-		
+		    	System.out.print(values[i][j]);		    	
+		    }		
 		}
 	}	
 	//Znajdowanie alfa reduct
-	public static void findReduct(String[][] valuesFavorble, String[][] valuesNegative){
+	public static void findReduct(String[][] valuesFavorble, String[][] valuesNegative){		
 		//iteruj bo ubu tablicach (oprucz pierwszej kolumny bo jest to decycja) i porownuj wartosci
 		StringBuffer temp = new StringBuffer();
 			for (int k=0; k<reductTable.length ; k++){
@@ -76,6 +84,51 @@ public class Main {
 				    }				    
 				}		
 			}
+	}
+	
+	public static void CNFtoDNF(){
+		ClassicalLogic.Utilities cl ;		
+		Formula f = null;
+	}
+	
+	public static void countReduct(){
+
+//		for (int i = 0; i < reductTable.length; i++) {
+//			for (int j = 0; j < reductTable[i].length; j++) {
+//				for (int k = 0; k < countReductMap.size(); k++) {
+//					if (countReductMap.get(k).equals(reductTable[i][j])){
+//						countReductMap.						
+//					}else{
+//						countReductMap.put(reductTable[i][j], 1);						
+//					}
+//				}							
+//			}			
+//		}		
+	}
+	//Wiem ze nie ladnie to wyglada
+	public static void removeSpace(String[][] value){	
+		for(int i = 0; i<value.length; i++){
+			for(int j = 0; j<value[i].length; j++){
+				value[i][j] = value[i][j].trim();
+				value[i][j] = value[i][j].replace("\r\n", " ");				
+				value[i][j] = value[i][j].replace("  ", " ");
+				value[i][j] = value[i][j].replace("   ", " ");
+				value[i][j] = value[i][j].replace("    ", " ");
+				value[i][j] = value[i][j].replace("     ", " ");
+				value[i][j] = value[i][j].replace("      ", " ");
+				value[i][j] = value[i][j].replace("       ", " ");
+				value[i][j] = value[i][j].replace("        ", " ");
+				value[i][j] = value[i][j].replace("         ", " ");
+				value[i][j] = value[i][j].replace("          ", " ");
+				value[i][j] = value[i][j].replace("           ", " ");
+				value[i][j] = value[i][j].replace("            ", " ");
+				value[i][j] = value[i][j].replace("             ", " ");
+				value[i][j] = value[i][j].replace("               ", " ");
+				value[i][j] = value[i][j].replace("  ", " ");
+		
+			}
+	       
+		}
 	}
 }
 
